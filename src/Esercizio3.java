98ccaff24f8b98c01bf7b78c0a1f09c0c3635e65
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-03-04 16:48:45 +0100

    penultimo esercizio fatto

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -2,9 +2,14 @@ public class Esercizio3 {
 
     public static void main(String[] args) {
           System.out.println("Il perimetro è:" + perimetroRettangolo( + 5,3));
+        System.out.println(pariDispari(10));
     }
     public static int perimetroRettangolo(int primoNumero, int secondoNumero){
         return (primoNumero + secondoNumero)*2;
     }
-
+    public  static int pariDispari(int n1){
+        if (n1 % 2 == 0) {
+return 0;
+        }else return 1;
+    }
 }
